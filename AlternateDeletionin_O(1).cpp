ae5b1049e6172c083674229c6deec35d589c9bf7
7rcksh@gmail.com <7rcksh@gmail.com>
2019-10-04 11:55:39 +0530

    Implemented max sumsub array in O(n).

--- AlternateDeletionin_O(1).cpp
+++ AlternateDeletionin_O(1).cpp
@@ -1,11 +1,15 @@
 // given - N- so 1 to N number, Alternate deletion of elements starting with 1st position and return last element left with this.
 
 #include <bits/stdc++.h>
+using namespace std;
 
 int main() {
   int N;
   cin >> N;
   vector <int> v(N);
-  iota(v.begin(), v.begin()+N, 1);    
+  iota(v.begin(), v.begin()+N, 1);
+  for (int i = 0; i < N; i++) {
+    cout << v[i] << " ";
+  }
   return 0;
 }
